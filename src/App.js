54f93f5ad56347c2e04c464231ce170ee7472c03
hakeem <hakeemforgit@gmail.com>
2024-02-16 12:25:19 +0530

    netflix done.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './app.css'
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import {original,action,horror,romance, comedy } from './url';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <RowPost title='Netflix Originals' url={original}/>
+      <RowPost title='Action' isSmall url={action}/>
+      <RowPost title='Horror' isSmall url={horror}/>
+      <RowPost title='Romance' isSmall url={romance}/>
+      <RowPost title='Comedy' isSmall url={comedy}/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div className='navbar'>
+      <img className="logo" src="https://logohistory.net/wp-content/uploads/2023/05/Netflix-Logo.png" alt="Netflix Logo" />
+      <img className="avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="avatar" />
+    </div>
+  )
+}
+
+export default NavBar
